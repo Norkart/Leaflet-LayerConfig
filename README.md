3c7ebb748e98afa3ce93ae511ec80a7b563c184f
Torbjørn Auglænd Vilhelmsen <torbjvi@stud.ntnu.no>
2014-06-24 09:10:54 +0200

    Edited readme. Added src files and sample json file

--- README.md
+++ README.md
@@ -2,3 +2,191 @@ leaflet-layerconfig
 ===================
 
 Library that communicates with a web service/reads a stored JavaScript object and adds layers to a leaflet map.
+
+
+
+##Usage
+L.LayerConfig takes two arguments, the first one is either a URL or a object containing the information about the layers to add. A sample.json file is provided that shows some of the options available.
+The second argument is the map container or LayerGroup/FeatureGroup on which we add layers to.
+
+```
+var layerConfig = L.layerConfig("layerconfig.json", map);
+```
+
+##JSON notation
+The object containing the information is pretty basic, here is a sample file adding only a marker.
+ ```
+{
+    
+    "layers": [
+        {
+            "type": "marker",
+            "latLng": [63.43048, 10.39508],
+            "popupContent": "Olavs statuen"
+        }
+    ]
+}
+ ```
+
+ To pass options to the layer simply add a options key to the layer. The options passed to the layer is in the same format as the LEaflet options found in the reference.
+ Some option keys support functions if you want to pass a function to Leaflet trough a web service you need to put it in a single line string. A minifier can help with that. This is because JSON does not support functions or multiline strings.
+
+ ###Other supported layer types:
+
+####GeoJSON
+This can either take an URL or a GeoJSON object
+
+```
+{
+         	"type": "geojson",
+         	"url": "http://example.com/geojson-example.geojson",
+         	"options": {
+         		"style": "function () { return { color: \"red\" }; };"
+         	}
+
+}
+```
+
+```
+{
+  "type": "geojson",
+  "geojson": {
+			  "type": "FeatureCollection",
+			  "features": [
+			    {
+			      "type": "Feature",
+			      "geometry": {
+			        "type": "Point",
+			        "coordinates": [102.0, 0.6]
+			      },
+			      "properties": {
+			        "prop0": "value0"
+			      }
+			    }
+			  ]
+			}
+
+}
+```
+####TileLayer
+```
+{
+    "type": "tilelayer",
+    "url": "http://b.tiles.mapbox.com/v3/torbjornav.hhni5j5f/{z}/{x}/{y}.png",
+    "options": {
+        "attribution": "Mapbox"
+    }
+}
+```
+####WMS
+```
+{
+    "type": "wms",
+    "url": "URL-TO-WMS",
+    "options": {
+        //WMS OPTIONS GO HERE
+    }
+}
+```
+####PolyLine
+```
+ {
+    "type": "line",
+    "path": [[63.43182, 10.39195],[63.42526, 10.3937]],
+    "popupContent": "Prinsens gate"
+ }
+```
+####Polygon
+```
+ {
+    "type": "polygon",
+    "path": [[[63.42639, 10.38975],[63.42564, 10.39006],[63.42570, 10.39194],[63.42643, 10.39183],[63.42639, 10.38975]]]
+ }
+```
+####Rectangle
+```
+ {
+    "type": "rectangle",
+    "path": [[63.42647, 10.39694],[63.42554, 10.39508]],
+    "options": {
+        
+            "color": "red"
+        
+    }
+ }
+```
+####Circle
+```
+{ 
+    "type": "circle",
+    "latLng": [63.43055, 10.39273],
+    "radius": 10
+}
+```
+####CircleMarker
+```
+{
+    "type": "circlemarker",
+    "latLng": [63.43034, 10.38647]
+}
+```
+
+####FeatureGroup
+FeatureGroup has a "layer" key containing the layers to be added to it. This is in the same format as the top-level layer key.
+```
+{
+    "type": "featuregroup",
+    "layers": [
+        {
+            "type": "marker",
+            "latLng": [63.43027, 10.40094]
+        },
+        { 
+            "type": "circle",
+            "latLng": [63.43055, 10.39273],
+            "radius": 10
+        },
+        {
+            "type": "circlemarker",
+            "latLng": [63.43034, 10.38647]
+        }
+
+    ],
+    "popupContent": "Kongens gate"
+}
+```
+####LayerGroup
+LayerGroup has a "layer" key containing the layers to be added to it. This is in the same format as the top-level layer key.
+```
+{
+    "type": "layergroup",
+    "layers": [
+        {
+            "type": "marker",
+            "latLng": [63.43027, 10.40094]
+        },
+        { 
+            "type": "circle",
+            "latLng": [63.43055, 10.39273],
+            "radius": 10
+        },
+        {
+            "type": "circlemarker",
+            "latLng": [63.43034, 10.38647]
+        }
+
+    ],
+    "popupContent": "Kongens gate"
+}
+```
+###Events
+Leaflet-LayerConfig fires various events.
+
+| Name | Description |
+| ---- | ----------- |
+| startLayerLoading |Fired when the JSON object is read and we're ready to start loading layers |
+| stopLayerLoading | Fired when all layers has been loaded |
+| LayerLoaded | Fired when a layer has been added. Passes the Leaflet layer, type of layer and the name of the layer with the event object |
+| GroupLoadingStart | Fired when we start loading layers to a group. Passes the group to the layer |
+| GroupLoadingEnd | Fired when we have loaded all sub-layers to a group |
+
---
+++ src/leaflet-layerconfig.js
@@ -0,0 +1,267 @@
+/*{
+	layers: [{
+
+	}]
+}*/
+
+L.LayerConfig = L.Class.extend(
+{
+	includes: L.Mixin.Events,
+	_map: null,
+	_json: null,
+	initialize: function(json, map) {
+		this._map = map;
+		this._json = json;
+		if(typeof json == "string") { // We take a string to mean we got an url;
+			var parent = this;
+			function success(data) {
+				parent.addLayers(JSON.parse(data));
+			};
+			this._ajaxRequest(json, success, function () {});
+		}
+		else {
+			this.addLayers(json);
+		}
+
+	},
+	_ajaxRequest: function (url, callback, onerror) {
+		var httpRequest;
+
+		  function makeRequest(url) {
+		    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+		      httpRequest = new XMLHttpRequest();
+		    } else if (window.ActiveXObject) { // IE
+		      try {
+		        httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+		      } 
+		      catch (e) {
+		        try {
+		          httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+		        } 
+		        catch (e) {}
+		      }
+		    }
+
+		    if (!httpRequest) {
+		      throw new Error('Giving up :( Cannot create an XMLHTTP instance');
+		      return false;
+		    }
+		    httpRequest.onreadystatechange = alertContents;
+		    httpRequest.open('GET', url);
+		    httpRequest.send();
+		  }
+
+		  function alertContents() {
+		    if (httpRequest.readyState === 4) {
+		      if (httpRequest.status === 200) {
+		        callback(httpRequest.responseText);
+		      } else {
+		        onerror(httpRequest);
+		      }
+		    }
+		  }
+		  makeRequest(url);
+	},
+	_evalJsonOptions: function (options) {
+		if (options && options.style && typeof options.style == "string") {
+			eval("options.style = "+options.style);
+		}
+		if (options && options.onEachFeature && typeof options.onEachFeature == "string") {
+			eval("options.onEachFeature = "+options.onEachFeature);
+		}
+		if (options && options.filter && typeof options.filter == "string") {
+			eval("options.filter = "+options.filter);
+		}
+		if (options && options.pointToLayer && typeof options.pointToLayer == "string") {
+			eval("options.pointToLayer = "+options.pointToLayer);
+		}
+		if (options && options.coordsToLatLng && typeof options.coordsToLatLng == "string") {
+			eval("options.coordsToLatLng = "+json.options.coordsToLatLng);
+		}
+		return options;
+	},
+	addLayers: function (json, addTo) {
+		if(!addTo)
+			addTo = this._map;
+		this.fireEvent("startLayerLoading");
+		for (var i = json.layers.length - 1; i >= 0; i--) {
+			var layer = json.layers[i];
+			switch (layer.type) {
+				case 'marker':
+				this.addMarker(layer, addTo);
+				break;
+				case 'geojson':
+				this.addGeoJson(layer, addTo);
+				break;
+				case 'layergroup':
+				this.addLayerGroup(layer, addTo);
+				break;
+				case 'featuregroup':
+				this.addFeatureGroup(layer, addTo);
+				break;
+				case 'tilelayer':
+				this.addTileLayer(layer, addTo);
+				break;
+				case 'wms':
+				this.addWMS(layer, addTo);
+				break;
+				case 'circlemarker':
+				this.addCircleMarker(layer, addTo);
+				break;
+				case 'circle':
+				this.addCircle(layer, addTo);
+				break;
+				case 'rectangle':
+				this.addRectangle(layer, addTo);
+				break;
+				case 'polygon':
+				this.addPolygon(layer, addTo);
+				break;
+				case 'line':
+				this.addLine(layer, addTo);
+				break;
+				case 'layer':
+				this.addLayer(layer, addTo);
+				break;
+			}
+
+		};
+		this.fireEvent("completeLayerLoading");
+		return this;
+	},
+	addMarker: function(json, addTo) {
+		json.options = this._evalJsonOptions(json.options);
+		var l = L.marker(json.latLng, json.options);
+		if(json.popupContent)
+			l.bindPopup(json.popupContent);
+		addTo.addLayer(l);
+		this.fireEvent("LayerLoaded", { layerType: "CircleMarker", name: json.name, layer: l });
+		return this;
+
+	},
+	
+	addGeoJson: function(json, addTo) {
+		json.options = this._evalJsonOptions(json.options);
+		var parent = this;
+		var addGeoJSON = function (geojson, options, map) {
+			var l = L.geoJson(geojson,options);
+			addTo.addLayer(l);
+			parent.fireEvent("LayerLoaded", { layerType: "GeoJSON", name: json.name, layer: l });
+		}
+		function onsuccess(data) {
+			addGeoJSON(JSON.parse(data),json.options,parent._map);
+		}
+		function onerror(data) {
+			console.debug(data);
+		}
+		if(json.url) {
+			this._ajaxRequest(json.url, onsuccess, onerror);
+		}
+		else if(json.geojson) {
+			addGeoJSON(json.geojson,json.options,this._map);
+		}
+		else {
+			throw new Error("URL or GeoJSON object expected");
+		}
+		return this;
+
+	},
+	addLayerGroup: function(json, addTo) {
+		json.options = this._evalJsonOptions(json.options);
+		var l = L.layerGroup();
+		this.fireEvent("GroupLoadingStart");
+		this.addLayers(json, l);
+		this.fireEvent("GroupLoadingEnd");
+		addTo.addLayer(l);
+		this.fireEvent("LayerLoaded", { layerType: "LayerGroup", name: json.name, layer: l });
+		return this;
+
+	},
+	addFeatureGroup: function(json, addTo) {
+		json.options = this._evalJsonOptions(json.options);
+		var l = L.featureGroup();
+		this.fireEvent("GroupLoadingStart");
+		this.addLayers(json, l);
+		this.fireEvent("GroupLoadingEnd");
+		if(json.popupContent)
+			l.bindPopup(json.popupContent);
+		addTo.addLayer(l);
+		this.fireEvent("LayerLoaded", { layerType: "FeatureGroup", name: json.name, layer: l });
+		return this;
+	},
+	addTileLayer: function (json, addTo) {
+		json.options = this._evalJsonOptions(json.options);
+		var l = L.tileLayer(json.url, json.options);
+		addTo.addLayer(l);
+		l.bringToFront();
+		this.fireEvent("LayerLoaded", { layerType: "TileLayer", name: json.name, layer: l });
+		return this;
+	},
+	addWMS: function (json, addTo) {
+		json.options = this._evalJsonOptions(json.options);
+		var l = L.tileLayer.wms(json.url, json.options);
+		addTo.addLayer(l);
+		l.bringToFront();
+		this.fireEvent("LayerLoaded", { layerType: "TileLayer.WMS", name: json.name, layer: l });
+		return this;
+	},
+	addCircleMarker: function (json, addTo) {
+		json.options = this._evalJsonOptions(json.options);
+		var l = L.circleMarker(json.latLng, json.options);
+		if(json.popupContent)
+			l.bindPopup(json.popupContent);
+		addTo.addLayer(l);
+		this.fireEvent("LayerLoaded", { layerType: "CircleMarker", name: json.name, layer: l });
+		return this;
+	},
+	addCircle: function (json, addTo) {
+		json.options = this._evalJsonOptions(json.options);
+		var l = L.circle(json.latLng, json.radius, json.options);
+		if(json.popupContent)
+			l.bindPopup(json.popupContent);
+		addTo.addLayer(l);
+		this.fireEvent("LayerLoaded", { layerType: "Circle", name: json.name, layer: l });
+		return this;
+	},
+	addRectangle: function (json, addTo) {
+		json.options = this._evalJsonOptions(json.options);
+		var l = L.rectangle(json.path, json.options);
+		if(json.popupContent)
+			l.bindPopup(json.popupContent);
+		addTo.addLayer(l);
+		this.fireEvent("LayerLoaded", { layerType: "Rectangle", name: json.name, layer: l });
+		return this;
+	},
+	addPolygon: function (json, addTo) {
+		json.options = this._evalJsonOptions(json.options);
+		var l = L.polygon(json.path, json.options);
+		if(json.popupContent)
+			l.bindPopup(json.popupContent);
+		addTo.addLayer(l);
+		this.fireEvent("LayerLoaded", { layerType: "Polygon", name: json.name, layer: l });
+		return this;
+	},
+	addLine: function (json, addTo) {
+		json.options = this._evalJsonOptions(json.options);
+		var l = L.polyline(json.path, json.options);
+		if(json.popupContent)
+			l.bindPopup(json.popupContent);
+		addTo.addLayer(l);
+		this.fireEvent("LayerLoaded", { layerType: "Line", name: json.name, layer: l });
+		return this;
+	},
+	addLayer: function (json, addTo) {
+		json.options = this._evalJsonOptions(json.options);
+		var l = json.layer;
+		if(json.popupContent)
+			l.bindPopup(json.popupContent);
+		addTo.addLayer(l);
+		this.fireEvent("LayerLoaded", { layerType: "Layer", name: json.name, layer: l });
+		return this;
+	}
+}
+);
+
+L.layerConfig = function(json, map) {
+	return new L.LayerConfig(json,map);
+};
